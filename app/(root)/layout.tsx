c7144599becfe9572f5411f978c637504c302926
VrundaKhunt <104060166+VrundaKhunt@users.noreply.github.com>
2024-03-25 23:37:06 +0530

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,32 @@
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
+import Script from "next/script";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <div className="flex h-screen flex-col">
-        <Header/>
-        <main className="flex-1">{children}</main>
-        <Footer/>
-      </div>
-    );
-  }
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Script src="//code.tidio.co/9pxzmbvbk5i0rc12uenspxpze6jebbet.js" >
+      </Script>
+
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7P8L3DE44X"></Script>
+      <Script>
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7P8L3DE44X');
+         `
+        }
+
+      </Script>
+      <Footer />
+    </div>
+  );
+}
